e1be3f7807b9821628a0d7b84bf2f06a731da136
Alexey <gotmanalexey@gmail.com>
2018-04-25 13:23:08 +0600

    init1

--- ScheduleKSTU/dbo/Tables/Week.sql
+++ ScheduleKSTU/dbo/Tables/Week.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Week] (
     [Id]         TINYINT IDENTITY (1, 1) NOT NULL,
     [Number]     TINYINT NOT NULL,
-    [Znamenatel] BIT     NOT NULL,
+    [Znamenatel] BIT       NOT NULL,
     CONSTRAINT [PK_Week] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
