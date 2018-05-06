014b36477ed8e590e33a5e78eccd7e393396741c
Sedokina <sedokina52@gmail.com>
2018-05-06 21:49:28 +0600

    Расчасовка:  добавлена "Группа"  добавлен семестр  добавлена кафедра  добавлен год  WeekID вынесен в другую таблицу  добавлена таблица в которой RaschasovkaID связана с WeekID

--- ScheduleKSTU/dbo/Tables/Raschasovka.sql
+++ ScheduleKSTU/dbo/Tables/Raschasovka.sql
@@ -6,13 +6,21 @@
     [SubjectWithTypeId]    BIGINT   NOT NULL,
     [TotalHoursForSemestr] SMALLINT NOT NULL,
     [TotalHoursForWeek]    TINYINT  NOT NULL,
-    [WeekId]               TINYINT  NOT NULL,
     [AuditoriumId]         SMALLINT NULL,
+    [GroupId]              BIGINT   NOT NULL,
+    [DepartmentId]         SMALLINT NOT NULL,
+    [SemesterId]           TINYINT  NOT NULL,
+    [YearId]               TINYINT  NOT NULL,
     CONSTRAINT [PK_Raschasovka] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Raschasovka_Auditorium] FOREIGN KEY ([AuditoriumId]) REFERENCES [dbo].[Auditorium] ([Id]),
     CONSTRAINT [FK_Raschasovka_Course] FOREIGN KEY ([CourseId]) REFERENCES [dbo].[Course] ([Id]) ON UPDATE CASCADE,
+    CONSTRAINT [FK_Raschasovka_Department] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[Department] ([Id]),
+    CONSTRAINT [FK_Raschasovka_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id]),
+    CONSTRAINT [FK_Raschasovka_Semesters] FOREIGN KEY ([SemesterId]) REFERENCES [dbo].[Semesters] ([Id]),
     CONSTRAINT [FK_Raschasovka_SubjectWithType] FOREIGN KEY ([SubjectWithTypeId]) REFERENCES [dbo].[SubjectWithType] ([Id]) ON UPDATE CASCADE,
     CONSTRAINT [FK_Raschasovka_Teacher] FOREIGN KEY ([TeacherId]) REFERENCES [dbo].[Teacher] ([Id]) ON UPDATE CASCADE,
-    CONSTRAINT [FK_Raschasovka_Week] FOREIGN KEY ([WeekId]) REFERENCES [dbo].[Week] ([Id]) ON UPDATE CASCADE
+    CONSTRAINT [FK_Raschasovka_Year] FOREIGN KEY ([YearId]) REFERENCES [dbo].[Years] ([Id])
 );
+
+
 
