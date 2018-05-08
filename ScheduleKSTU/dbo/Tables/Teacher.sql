0263ac8b576ec094ebf12e2ea6f40a3f8e933844
Sedokina <sedokina52@gmail.com>
2018-05-08 15:58:44 +0600

    Связь с кафедрой удалена из таблицы Teacher Добавлена таблица отдельная таблица для связи кафедры с преподавателем

--- ScheduleKSTU/dbo/Tables/Teacher.sql
+++ ScheduleKSTU/dbo/Tables/Teacher.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Teacher] (
-    [Id]           INT           IDENTITY (1, 1) NOT NULL,
-    [FirstName]    NVARCHAR (40) NULL,
-    [LastName]     NVARCHAR (40) NULL,
-    [DepartmentId] SMALLINT      NULL,
-    CONSTRAINT [PK_Teacher] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Teacher_Department] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[Department] ([Id]) ON UPDATE CASCADE
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (40) NULL,
+    [LastName]  NVARCHAR (40) NULL,
+    CONSTRAINT [PK_Teacher] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
---
+++ ScheduleKSTU/dbo/Tables/TeacherDepartment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TeacherDepartment] (
+    [Id]           INT      IDENTITY (1, 1) NOT NULL,
+    [TeacherId]    INT      NOT NULL,
+    [DepartmentId] SMALLINT NOT NULL,
+    CONSTRAINT [PK_TeacherDepartmentId] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TeacherDepartment_Department] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[Department] ([Id]),
+    CONSTRAINT [FK_TeacherDepartment_Teacher] FOREIGN KEY ([TeacherId]) REFERENCES [dbo].[Teacher] ([Id])
+);
+
