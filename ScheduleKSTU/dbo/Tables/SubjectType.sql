bdaf3b17d60ee403584388b724028de4e94cdead
Sedokina <sedokina52@gmail.com>
2018-05-09 16:39:23 +0600

    Обновлена таблица SubjectType Добавлено полное названия типа предмета

--- ScheduleKSTU/dbo/Tables/SubjectType.sql
+++ ScheduleKSTU/dbo/Tables/SubjectType.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[SubjectType] (
-    [Id]   TINYINT       IDENTITY (1, 1) NOT NULL,
-    [Name] NVARCHAR (30) NOT NULL,
+    [Id]       TINYINT       IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (10) NULL,
+    [FullName] NCHAR (40)    NULL,
     CONSTRAINT [PK_SubjectType] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
