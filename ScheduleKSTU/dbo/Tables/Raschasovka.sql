8821e01576a1fff48f28af7337e7b1e6759e705e
Sedokina <sedokina52@gmail.com>
2018-05-09 22:02:27 +0600

    Удалена таблица SubjectWithType Из расчасовки удалена связь с таблицей SubjectWithType, добавлена связь с Subject и SubjectType Из Schedule удалена связь с таблицей SubjectWithType, добавлена связь с Subject и SubjectType Из Subject удалена связь с Department Добавлена таблица SubjectDepartment для связи Subject с Department

--- ScheduleKSTU/dbo/Tables/Raschasovka.sql
+++ ScheduleKSTU/dbo/Tables/Raschasovka.sql
@@ -3,24 +3,27 @@
     [Potok]                INT      NOT NULL,
     [CourseId]             TINYINT  NOT NULL,
     [TeacherId]            INT      NOT NULL,
-    [SubjectWithTypeId]    BIGINT   NOT NULL,
     [TotalHoursForSemestr] SMALLINT NOT NULL,
-    [TotalHoursForWeek]    TINYINT  NOT NULL,
     [AuditoriumId]         SMALLINT NULL,
     [GroupId]              BIGINT   NOT NULL,
     [DepartmentId]         SMALLINT NOT NULL,
     [SemesterId]           TINYINT  NOT NULL,
     [YearId]               TINYINT  NOT NULL,
+    [SubjectId]            BIGINT   NOT NULL,
+    [SubjectTypeId]        TINYINT  NOT NULL,
     CONSTRAINT [PK_Raschasovka] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Raschasovka_Auditorium] FOREIGN KEY ([AuditoriumId]) REFERENCES [dbo].[Auditorium] ([Id]),
     CONSTRAINT [FK_Raschasovka_Course] FOREIGN KEY ([CourseId]) REFERENCES [dbo].[Course] ([Id]) ON UPDATE CASCADE,
     CONSTRAINT [FK_Raschasovka_Department] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[Department] ([Id]),
     CONSTRAINT [FK_Raschasovka_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id]),
     CONSTRAINT [FK_Raschasovka_Semesters] FOREIGN KEY ([SemesterId]) REFERENCES [dbo].[Semesters] ([Id]),
-    CONSTRAINT [FK_Raschasovka_SubjectWithType] FOREIGN KEY ([SubjectWithTypeId]) REFERENCES [dbo].[SubjectWithType] ([Id]) ON UPDATE CASCADE,
+    CONSTRAINT [FK_Raschasovka_Subject] FOREIGN KEY ([SubjectId]) REFERENCES [dbo].[Subject] ([Id]),
+    CONSTRAINT [FK_Raschasovka_SubjectType] FOREIGN KEY ([SubjectTypeId]) REFERENCES [dbo].[SubjectType] ([Id]),
     CONSTRAINT [FK_Raschasovka_Teacher] FOREIGN KEY ([TeacherId]) REFERENCES [dbo].[Teacher] ([Id]) ON UPDATE CASCADE,
     CONSTRAINT [FK_Raschasovka_Year] FOREIGN KEY ([YearId]) REFERENCES [dbo].[Years] ([Id])
 );
+
+
 
 
 
---
+++ ScheduleKSTU/dbo/Tables/SubjectDepartment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SubjectDepartment] (
+    [Id]           BIGINT   IDENTITY (1, 1) NOT NULL,
+    [SubjectId]    BIGINT   NOT NULL,
+    [DepartmentId] SMALLINT NULL,
+    CONSTRAINT [PK_SubjectDepartmentId] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_SubjectDepartment_Department] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[Department] ([Id]),
+    CONSTRAINT [FK_SubjectDepartment_Subject] FOREIGN KEY ([SubjectId]) REFERENCES [dbo].[Subject] ([Id])
+);
+
