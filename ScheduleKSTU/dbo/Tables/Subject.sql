a23172ee895ff5a2a7a7e2e12b1f5f9efc685762
Sedokina <sedokina52@gmail.com>
2018-05-10 16:06:44 +0600

    Изменение размера поля FullName для Subject

--- ScheduleKSTU/dbo/Tables/Subject.sql
+++ ScheduleKSTU/dbo/Tables/Subject.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[Subject] (
-    [Id]       BIGINT        IDENTITY (1, 1) NOT NULL,
-    [Name]     NVARCHAR (15) NOT NULL,
-    [FullName] NVARCHAR (50) NOT NULL,
+    [Id]       BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (15)  NULL,
+    [FullName] NVARCHAR (150) NOT NULL,
     CONSTRAINT [PK_Subject] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
