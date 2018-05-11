afa1fc403062c507c553870fec1f202f311f3ca1
Sedokina <sedokina52@gmail.com>
2018-05-11 09:29:56 +0600

    Добавлена количество часов в неделю для недель расчаовки

--- ScheduleKSTU/dbo/Tables/RaschasovkaWeeks.sql
+++ ScheduleKSTU/dbo/Tables/RaschasovkaWeeks.sql
@@ -2,8 +2,11 @@
     [Id]            BIGINT  IDENTITY (1, 1) NOT NULL,
     [RaschasovkaId] INT     NOT NULL,
     [WeekId]        TINYINT NOT NULL,
+    [HoursForWeek]  TINYINT NOT NULL,
     CONSTRAINT [PK_RaschasovkaWeeksId] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_RaschasovkaWeeks_Raschasovka] FOREIGN KEY ([RaschasovkaId]) REFERENCES [dbo].[Raschasovka] ([Id]),
     CONSTRAINT [FK_RaschasovkaWeeks_Week] FOREIGN KEY ([WeekId]) REFERENCES [dbo].[Week] ([Id])
 );
+
+
 
